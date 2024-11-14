359c1f84163fbc4fb245e0a88eed2c4511b0473a
skatistics <131618960+skatistics@users.noreply.github.com>
2024-11-14 14:36:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "https://skatistics.github.io/portfolio-v2/",
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
 });
