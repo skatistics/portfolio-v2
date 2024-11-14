910caf3abb6a469730a007eb608f73c994d9a56b
skatistics <131618960+skatistics@users.noreply.github.com>
2024-11-14 21:32:53 +0800

    for deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,6 +54,7 @@ const Home = () => {
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-v2/dist/",
+  base: "./",
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
 });
